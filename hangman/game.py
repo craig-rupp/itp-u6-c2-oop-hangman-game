c4d501e7427d0229a6296dbd268f530e28d913fe
Craig Rupp <craigrupp@utexas.edu>
2018-07-05 21:12:57 +0000

    Initial Testing

--- hangman/game.py
+++ hangman/game.py
@@ -1,13 +1,32 @@
 from .exceptions import *
-
+import random
 
 class GuessAttempt(object):
     pass
 
 
 class GuessWord(object):
-    pass
+    
+    def __init__(self, passed_word):
+        self.answer = passed_word
+        self.masked = '*' * len(passed_word)
 
 
 class HangmanGame(object):
-    pass
+    
+    WORD_LIST = ['rmotr', 'python', 'awesome']
+    
+    def __init__(self, word_list=None, number_of_guesses=5):
+        self.word_list = HangmanGame.WORD_LIST if not word_list else word_list
+        self.previous_guesses = []
+        self.remaining_misses = number_of_guesses
+        self.word = GuessWord(self.select_random_word(word_list))
+    
+    @classmethod   
+    def select_random_word(cls, list_of_words):
+        if list_of_words == []:
+            raise InvalidListOfWordsException()
+        return random.choice(list_of_words)
+
+
+
